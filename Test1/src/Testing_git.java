df665f7d65d9616b5429482a27ca14fa90bddefd
Laila Alhmoud <tflm1014@gmail.com>
2016-04-01 22:55:22 -0400

    Testing again

--- Test1/src/Testing_git.java
+++ Test1/src/Testing_git.java
@@ -5,6 +5,8 @@ public class Testing_git {
 		// TODO Auto-generated method stub
 		System.out.println("I'm tetsing git settings.");
 		System.out.println("Not working.");
+		System.out.println("Testing..");
+
 	}
 
 }
