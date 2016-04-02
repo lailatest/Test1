eaf8417ecf2f8cf31ce8adb43b892a7418135194
Laila Alhmoud <tflm1014@gmail.com>
2016-04-01 22:41:31 -0400

    Still testing

--- Test1/src/Testing_git.java
+++ Test1/src/Testing_git.java
@@ -4,6 +4,7 @@ public class Testing_git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm tetsing git settings.");
+		System.out.println("Not working.");
 	}
 
 }
