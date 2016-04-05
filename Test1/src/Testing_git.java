63fcd2ac616ba037837a95c8dc1d82a8b481ad27
Laila Alhmoud <tflm1014@gmail.com>
2016-04-05 10:40:16 -0400

    Testing with configurations.

--- Test1/src/Testing_git.java
+++ Test1/src/Testing_git.java
@@ -2,7 +2,7 @@ public class Testing_git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Version 12.");
+		System.out.println("Version 13.");
 
 	}
 
