1e9b3da3513a6e017ce9d0e546c5ae85741c76a4
Laila Alhmoud <tflm1014@gmail.com>
2016-04-05 12:30:02 -0400

    End to end test 12:29

--- Test1/src/Testing_git.java
+++ Test1/src/Testing_git.java
@@ -2,7 +2,7 @@ public class Testing_git {
 //Test 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Version 21");
+		System.out.println("Version 22");
 		//Print to debug
 	}
 
